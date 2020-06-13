1ae84a6130538744923da9971bcae71af15c6a6e
Firzlaff <tysenfirzlaff@gmail.com>
2020-06-13 10:55:47 -0600

    adding db info for workouts and tips, schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,30 @@
--- Drops database if it is already there
-DROP DATABASE IF EXISTS project2_db;
--- Creates the database after its dropped. 
-CREATE DATABASE project2_db;
+-- -- Drops database if it is already there
+-- DROP DATABASE IF EXISTS project2_db;
+-- -- Creates the database after its dropped. 
+-- CREATE DATABASE project2_db;
 -- Start the database
-USE project2_db;
--- Creating the tables for notes
+USE dfbs7vcu2m1nh29q;
+-- Creating the table for notes/todo section
 CREATE TABLE notes (
     id INT NOT NULL AUTO_INCREMENT,
     note TEXT NOT NULL,
     PRIMARY KEY (id)
+);
+
+
+
+USE dfbs7vcu2m1nh29q;
+-- Creating the table for the workout section
+CREATE TABLE workouts (
+    day VARCHAR(50),
+    workout TEXT NOT NULL,
+    PRIMARY KEY (day)
+);
+
+USE dfbs7vcu2m1nh29q;
+-- Creating the table for the workout section
+CREATE TABLE tips (
+    name VARCHAR(100),
+    tip TEXT NOT NULL,
+    PRIMARY KEY (name)
 );
