273c7cbd25ec58e183a1f8bcb954b478ce291af8
Senad <senadutah@gmail.com>
2020-08-03 22:07:02 -0600

    added connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,15 @@
 const mysql = require("mysql");
 // requiring the .env file to secure protected data with environmental variables
 require("dotenv").config();
-
+var heroku = process.env.JAWSDB_URL;
 // connects to the deployed Heroku Database
-const connection = mysql.createConnection({
-   host: "qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-   user: "oaw9hcazqkwi0fd7",
-   password: "opcanhk0crezoflj",
-   port: 3306,
-   database: "dfbs7vcu2m1nh29q"
-});
+   const connection = mysql.createConnection(heroku);
+   // const connection = mysql.createConnection({
+//    host: "qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//    user: "oaw9hcazqkwi0fd7",
+//    password: "opcanhk0crezoflj",
+//    port: 3306,
+//    database: "dfbs7vcu2m1nh29q"
+// });
 
 module.exports = connection;
